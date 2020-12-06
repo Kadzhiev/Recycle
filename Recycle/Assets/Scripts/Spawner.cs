ea7c3277a9dbb68e7f9a878fa6ab5f5259d5da2f
Kadzhiev <akadzhiev@gmail.com>
2020-12-06 12:11:20 +0300

    done

---
+++ Recycle/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Stuff[] _template;
+    [SerializeField] private Transform _spawnPoint;
+    private bool _isFallnig;
+    private Stuff _object = null;
+
+    private void Start()
+    {
+        _isFallnig = false;
+    }
+    private void Update()
+    {
+        while(!_isFallnig)
+        {
+            _object = Instantiate(_template[Random.Range(0, 4)], _spawnPoint.position + new Vector3(Random.Range(-9, 9),0, Random.Range(-9, 9)), Quaternion.identity);
+            _isFallnig = true;
+        }
+        
+        if (_object && _object.IsColliding)
+        {
+            GameObject.Destroy(_object.gameObject);
+            _isFallnig = false;
+        }
+        if (_object && _object.IsOnFloor)
+        {
+            _isFallnig = false;
+        }
+    }
+}
---
+++ Recycle/Assets/Scripts/Stuff.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stuff : MonoBehaviour
+{
+    private bool _isColliding;
+    public bool IsColliding
+    {
+        get
+        {
+            return _isColliding;
+        }
+    }
+    private bool _isOnFloor;
+    public bool IsOnFloor
+    {
+        get
+        {
+            return _isOnFloor;
+        }
+    }
+
+
+    private void Start()
+    {
+        _isColliding = false;
+        _isOnFloor = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "recycle bin")
+        {
+            _isColliding = true;
+            _isOnFloor = false;
+        }
+        else
+        {
+            _isColliding = false;
+            _isOnFloor = true;
+        }
+    }
+}
